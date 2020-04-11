c81bddb7317c9d575f6a8231cb0022464fa7a0c2
callumboaden <boaden.callum@gmail.com>
2020-04-11 16:06:54 +0700

    add logic to recalculate player score if ace in hand

--- src/js/Hand.js
+++ src/js/Hand.js
@@ -7,11 +7,30 @@ class Hand {
         this.win = '';
     }
     calculateScore() {
+        let hasAce = false;
         let score = 0;
 
         for (let i = 0; i < this.cards.length; i++) {
+            
+            this.cards[i].value === "A" ? hasAce = true : false;
+
             score += this.cards[i].weight;
         }  
+
+        if (hasAce && score > 21) {
+            score = 0
+            // Recalculate score 
+            for (let i = 0; i < this.cards.length; i++) {
+                if (this.cards[i].value === "A") {
+                    this.cards[i].weight = 1;
+                }
+
+                score += this.cards[i].weight;
+            }
+
+        }
+
+
 
         this.score = score;
     }
